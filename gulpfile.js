2ceae46ffd4754ff009ec3299d9600a3f600b12e
Edward O'Reilly <ed0wolf@gmail.com>
2014-10-05 12:06:01 +0100

    addint watch-tests to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
-gulp.task('default', function() {
+gulp.task('tests', function() {
 	return gulp.src('test/**/*Tests.js', {read: false})
 		.pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('watch-tests', function() {
+	gulp.watch(['src/**', 'test/**'], ['tests']);
 });
