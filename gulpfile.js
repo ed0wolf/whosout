be1d33c3a9f66c1a2c2bd121170aec81c531f074
Edward O'Reilly <ed0wolf@gmail.com>
2014-10-11 15:14:27 +0100

    Adding bower and using it to bring in chosen - adding a template and bringin in the css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var gulp = require('gulp'),
+	mocha = require('gulp-mocha'),
+	usemin = require('gulp-usemin'),
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	rimraf = require('gulp-rimraf');
 
 gulp.task('tests', function() {
 	return gulp.src('test/**/*Tests.js', {read: false})
@@ -8,4 +13,21 @@ gulp.task('tests', function() {
 
 gulp.task('watch-tests', function() {
 	gulp.watch(['src/**', 'test/**'], ['tests']);
+});
+
+gulp.task('template', ['minify'], function() {
+	return gulp.src('src/public/layout.src.hbs')
+		.pipe(rimraf())
+		.pipe(rename('layout.hbs'))
+		.pipe(gulp.dest('src/views'));
+});
+
+gulp.task('minify', function() {
+	return gulp.src('src/views/layout.src.hbs')
+		.pipe(usemin({
+			assetsDir: './',
+			css: [minifyCss(), 'concat'],
+			js: [uglify(), 'concat']
+		}))
+		.pipe(gulp.dest('src/public'));
 });
